3b258f084c5232909f4792cc70d3a7a43ff0b324
OZER-Deniz <deniz.ozer@edu.univ-fcomte.fr>
2024-12-13 12:47:23 +0100

    réalisation du menu

---
+++ src/js/menu.js
@@ -0,0 +1,21 @@
+const menuBtn = document.querySelector('.menu-btn');
+const overlayMenu = document.querySelector('.overlay-menu');
+const closeBtn = document.querySelector('.close-btn');
+const body = document.body;
+
+menuBtn.addEventListener('click', () => {
+    overlayMenu.classList.add('active');
+    body.classList.add('no-scroll');
+});
+
+closeBtn.addEventListener('click', () => {
+    overlayMenu.classList.remove('active');
+    body.classList.remove('no-scroll');
+});
+
+document.addEventListener('click', (event) => {
+    if (!overlayMenu.contains(event.target) && !menuBtn.contains(event.target) && overlayMenu.classList.contains('active')) {
+        overlayMenu.classList.remove('active');
+        body.classList.remove('no-scroll');
+    }
+});
